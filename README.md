447354860c07ac19f40538afa7fa0628c20ac508
Kartikeya Choudhary <kartikeyachoudhary11@gmail.com>
2021-08-27 12:10:10 +0530

    feat
    added javascript and readme

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # profit-loss-calculator
- 
+
+Enter the buying price, selling price and the number of units to know whether you are making a profit or not
---
+++ app.js
@@ -0,0 +1,31 @@
+const initialPriceInput = document.querySelector('#initial-price');
+const stockQtyInput = document.querySelector('#stock-qty');
+const currentPriceInput = document.querySelector('#current-price');
+const calcBtn = document.querySelector('.calc-btn');
+const output = document.querySelector('.output-text');
+
+function calculate() {
+	output.innerText = '';
+	if (
+		initialPriceInput.value !== '' &&
+		stockQtyInput.value !== '' &&
+		currentPriceInput.value !== ''
+	) {
+		initialPrice = Number(initialPriceInput.value);
+		stockQty = Number(stockQtyInput.value);
+		currentPrice = Number(currentPriceInput.value);
+		const stockReturn = Math.abs((currentPrice - initialPrice) * stockQty);
+		const percentReturn = Math.abs((stockReturn / initialPrice) * 100).toFixed(2);
+		if (initialPrice > currentPrice) {
+			output.innerText = `Sorry you suffered a loss of ${stockReturn} and the percent is -${percentReturn} %`;
+		} else if (initialPrice === currentPrice) {
+			output.innerText = `Congrats! You broke even`;
+		} else {
+			output.innerText = `Hey, your profit is ${stockReturn} and the percent is ${percentReturn} %`;
+		}
+	} else {
+		return (output.innerText = 'Enter valid number in all fields');
+	}
+}
+
+calcBtn.addEventListener('click', calculate);
